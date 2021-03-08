9afdc85de6744b823ff91b43b2c51e9b779a982f
György Császár <csaszar.gyorgy890322@gmail.com>
2021-03-08 21:02:40 +0100

    Add WPFAccessibility.Test project

---
+++ WPFAccessibility.Test/UITests/WPFAccessibilityTest.cs
@@ -0,0 +1,78 @@
+﻿using Interop.UIAutomationClient;
+using NUnit.Framework;
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace WPFAccessibility.Test.UITests
+{
+    [TestFixture]
+    public class WPFAccessibilityTest
+    {
+        private Process _application;
+        private IUIAutomation _automation;
+        private IUIAutomationElement _mainWindow;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var psi = new ProcessStartInfo(@".\..\..\..\..\WPFAccessibility\bin\Debug\WPFAccessibility.exe")
+            {
+                WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory
+            };
+
+            // Start application
+            _application = Process.Start(psi);
+
+            // Wait for application main window
+            while (_application.MainWindowHandle == IntPtr.Zero)
+            {
+                Thread.Sleep(100);
+            }
+
+            // Create UIAutomation (UIA3) entry point
+            _automation = new CUIAutomation8();
+            _mainWindow = _automation.ElementFromHandle(_application.MainWindowHandle);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _application.CloseMainWindow();
+            _application.WaitForExit();
+        }
+
+        [Test]
+        [Description("ValuePattern is available, current value and read-only state is available, button text can be changed.")]
+        public void TestThatCustomButtonControlHasValuePattern()
+        {
+            // Find the custom button
+            var customButton = _mainWindow.FindFirst(TreeScope.TreeScope_Children, _automation.CreatePropertyCondition(UIA_PropertyIds.UIA_AutomationIdPropertyId, "customButtonId"));
+            Assert.IsNotNull(customButton);
+
+            // Get Value pattern
+            var valuePattern = customButton.GetCurrentPattern(UIA_PatternIds.UIA_ValuePatternId) as IUIAutomationValuePattern;
+            Assert.IsNotNull(valuePattern);
+
+            // Verify text and read-only state
+            Assert.AreEqual("CustomButtonText", valuePattern.CurrentValue);
+            Assert.IsTrue(valuePattern.CurrentIsReadOnly == 0);
+
+            // Set button text and verify
+            valuePattern.SetValue("ModifiedText");
+            Assert.AreEqual("ModifiedText", valuePattern.CurrentValue);
+        }
+
+        [Test]
+        [Description("Custom button control has information about text alignment.")]
+        public void TestThatCustomButtonControlTextAlignStateCanBeRead()
+        {
+            // Find the custom button
+            var customButton = _mainWindow.FindFirst(TreeScope.TreeScope_Children, _automation.CreatePropertyCondition(UIA_PropertyIds.UIA_AutomationIdPropertyId, "customButtonId"));
+            Assert.IsNotNull(customButton);
+
+            // Verify TextAlign
+            Assert.IsTrue(customButton.CurrentHelpText.Contains("HorizontalAlignment=Left;VerticalAlignment=Top"));
+        }
+    }
+}
